13016327cda68c170ce1cbc672d779999bb8fc26
vvast-e <123ast321@gmail.com>
2025-06-14 21:52:51 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app.include_router(tasks.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # URL фронтенда
+    allow_origins=["https://reputation-ecommerce.ru"],  # URL фронтенда
     allow_methods=["*"],
     allow_headers=["*"],
 )
