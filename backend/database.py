3cfdd771d29d1f793e2f37105c7bb1c4fe3bdff1
vvast-e <123ast321@gmail.com>
2025-06-15 01:07:37 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import backend.config
+import config
 
 Base=declarative_base()
-engine=create_async_engine(backend.config.settings.POSTGRES_URL)
+engine=create_async_engine(config.settings.POSTGRES_URL)
 AsyncSessionLocal=sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 async def get_db():
