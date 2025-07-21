23201444c7aef63cd694f187a383981d33b8c0bf
vvast-e <123ast321@gmail.com>
2025-07-21 15:01:11 +0300

    Update ozon_selenium_parser.py

--- utils/ozon_selenium_parser.py
+++ utils/ozon_selenium_parser.py
@@ -145,8 +145,6 @@ def start_driver():
     options.add_argument('--incognito')
     options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36')
     options.add_argument('--window-size=1920,1080')
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
     options.add_argument('--disable-infobars')
     options.add_argument('--disable-blink-features=AutomationControlled')
     print("Chrome options:", options.arguments)
