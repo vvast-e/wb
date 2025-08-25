64df74fe9da352f401128b4dbf99ddba5b287c5d
vvast-e <123ast321@gmail.com>
2025-08-25 22:10:43 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,6 +4,21 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 });
 
+// Добавляем interceptor для установки Authorization заголовка
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers = config.headers || {};
+            if (!config.headers['Authorization']) {
+                config.headers['Authorization'] = `Bearer ${token}`;
+            }
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
 // Добавляем interceptor для обработки 401
 api.interceptors.response.use(
     response => response,
